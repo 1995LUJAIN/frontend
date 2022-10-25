96c4104e5729f5b88f6b23d32aa64118999dbcc8
lujain.alshaikh <lujainalshaikh95@gmail.com>
2022-10-25 12:00:52 +0300

    assignment19java

---
+++ javascript/javascript.js
@@ -0,0 +1,19 @@
+
+
+
+
+let array=[1,2,3,4,5]
+onload=function(){
+    array.length
+    document.querySelector("h1").style.color='red';
+    document.getElementById("p").style.fontsize="30px";
+    document.querySelector("h2").style.color='blue';
+    document.querySelector("h3").style.color='yellow';
+    
+    document.getElementsByClassName("p")[0].style.fontsize="100px" ;
+    document.getElementsByClassName("p")[1].style.color="red" ;
+    
+   
+
+}
+
